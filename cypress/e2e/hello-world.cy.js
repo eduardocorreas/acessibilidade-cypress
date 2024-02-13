2358bcb5c1bf5fcd2045e1e714ed8059a301dcf7
Eduardo Correa <luiz.correa@grancursosonline.com.br>
2024-02-13 20:17:33 -0300

    first commit

---
+++ cypress/e2e/hello-world.cy.js
@@ -0,0 +1,34 @@
+/// <reference types="Cypress" />
+
+import { injectAxe } from "cypress-axe";
+
+describe('Aprendendo cypress básico',() =>{
+  it('Imagens possuem atributo alt', () => {
+    cy.visit('https://www.w3schools.com/w3css/tryw3css_templates_gourmet_catering.htm');
+    cy.get('img').then(image => {
+      expect(image).have.attr('alt')
+    })
+  })
+  
+  it('Contem H1', ()=>{
+    cy.visit('https://www.w3schools.com/w3css/tryw3css_templates_gourmet_catering.htm');
+    cy.get('h1').should('be.visible');
+  })
+  
+  it('Contem Main', ()=>{
+    cy.visit('https://www.w3schools.com/w3css/tryw3css_templates_gourmet_catering.htm');
+    cy.get('main').should('be.visible');
+  })
+
+  it('Validação total de acessibilidade', ()=>{
+    cy.visit('https://www.w3schools.com/w3css/tryw3css_templates_gourmet_catering.htm');
+    injectAxe();
+    cy.checkA11y();
+  })
+
+  it.only('Validação total de acessibilidade', ()=>{
+    cy.visit('https://www.w3schools.com/w3css/tryw3css_templates_gourmet_catering.htm');
+    //cy.pageAccessibility();
+    cy.componentAccessibility('h1');
+  })
+})
